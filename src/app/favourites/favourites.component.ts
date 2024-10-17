ac01ed0aa85807a0f39574cbd31f0f11a201dda7
Petros X. <petrosxsp@gmail.com>
2024-10-17 13:20:36 +0300

    local Storage

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 import { Photo, RANDOM_PHOTOS } from '../random-photos';
 import { PhotoComponent } from '../photo/photo.component';
+import { LocalStorageService } from './local-storage.service';
 
 @Component({
   selector: 'app-favorites',
@@ -10,9 +12,30 @@ import { PhotoComponent } from '../photo/photo.component';
   styleUrl: './favourites.component.css',
 })
 export class FavouritesComponent {
-  likedphotos: Photo[] = [];
+  likedPhotos: Photo[] = [];
 
-  constructor() {
-    this.likedphotos = RANDOM_PHOTOS.filter((photo) => photo.liked === true);
+  // constructor() {
+  //   this.likedphotos = RANDOM_PHOTOS.filter((photo) => photo.liked === true);
+  // }
+
+  constructor(private localStorageService: LocalStorageService) {
+    const savedPhotos = this.localStorageService.getItem('likedPhotos');
+    if (savedPhotos) {
+      this.likedPhotos = JSON.parse(savedPhotos);
+    } else {
+      this.likedPhotos = RANDOM_PHOTOS.filter((photo) => photo.liked === true);
+    }
+  }
+
+  updateLikedPhotos() {
+    const photosString = JSON.stringify(this.likedPhotos);
+    this.localStorageService.setItem('likedPhotos', photosString);
+  }
+
+  onPhotoLike(photo: Photo) {
+    // Toggle liked status, update the likedPhotos array, and save it
+    photo.liked = !photo.liked;
+    this.likedPhotos = RANDOM_PHOTOS.filter((photo) => photo.liked === true);
+    this.updateLikedPhotos();
   }
 }
